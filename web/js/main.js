3eebef615ae2b7d75979f363775241be478b8968
Moritz Friedrich <m@9dev.de>
2017-12-15 19:03:45 +0100

    Updated design

--- web/js/main.js
+++ web/js/main.js
@@ -1,10 +1,42 @@
-/**
- * Created by andy on 11.07.17.
+'use strict';
+
+/*
+ global window
  */
 
+window.document.addEventListener('DOMContentLoaded', init);
 
 function confirmAction($item, $actionUrl) {
-    if (confirm('Are you sure you want to delete ' + $item + '?')) {
-        window.location.href = $actionUrl;
-    }
+  if (confirm('Are you sure you want to delete ' + $item + '?')) {
+    window.location.href = $actionUrl;
+  }
+}
+
+function init() {
+  Array.from(window.document.querySelectorAll('.action.delete')).forEach(button => {
+    button.addEventListener('click', event => confirmItemDeletion(event));
+  });
+}
+
+function confirmItemDeletion(event) {
+  event.preventDefault();
+
+  const button = event.target,
+        label  = button.textContent;
+
+  if (button.hasOwnProperty('deletionConfirmed') && button.deletionConfirmed) {
+    window.location.href = button.getAttribute('href');
+
+    return true;
+  }
+
+  button.classList.add('active');
+  button.textContent       = 'Really?';
+  button.deletionConfirmed = true;
+
+  setTimeout(() => {
+    button.classList.remove('active');
+    button.textContent       = label;
+    button.deletionConfirmed = false;
+  }, 3000);
 }
